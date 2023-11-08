c215d83ab5b052de990dd950621978067335ce5d
YelyzavetaRozhdiestvenska <alirr@ukr.net>
2023-11-08 16:14:06 +0100

    new variant

--- src/components/statistic/Statistic.jsx
+++ src/components/statistic/Statistic.jsx
@@ -1,41 +1,14 @@
 import React from 'react';
-import { Notification } from 'components/notification/Notification';
-import {P} from './Statistic.style';
+import { P } from './Statistic.style';
 
-export function Statistic ({ feedback }) {
-
-  const countTotalFeedback = (feedback) => {
-    let sum = 0;
-    Object.values(feedback).forEach(value => sum += value);
-    return sum;
-  };
-
-  const countPositiveFeedbackPercentage = (feedback) => {
-    const goodToAll = (feedback.good / (countTotalFeedback(feedback)))
-    const percentage = (Math.round(goodToAll * 100));
-
-    if (isNaN(percentage)) {
-      return 0;
-    }
-
-    return percentage;
-
-  };
-
-  let total = countTotalFeedback(feedback);
-  let percentage = countPositiveFeedbackPercentage(feedback);
-
-  return (<>{total === 0 ? <Notification message="There is no feedback"/> : <FeedbackBlock feedback={feedback} total={total} percentage={percentage}/>}</>)
-
-  }
-
-  
-function FeedbackBlock ({ feedback, total, percentage }) {
-      return <>
-        <P>Good: {feedback.good}</P>
-        <P>Neutral: {feedback.neutral}</P>
-        <P>Bad: {feedback.bad}</P>
-        <P>Total: {total} </P>
-        <P>Positive feedback: {percentage}  %</P>
-      </>
-  }
+export function Statistic({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <>
+      <P>Good: {good}</P>
+      <P>Neutral: {neutral}</P>
+      <P>Bad: {bad}</P>
+      <P>Total: {total()} </P>
+      <P>Positive feedback: {positivePercentage()} %</P>
+    </>
+  );
+}
